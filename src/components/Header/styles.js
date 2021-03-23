aabde6c17b0dfac4ef298678a2b60d2bc9d28f01
seockhun <hun00719@gmail.com>
2021-03-23 22:43:12 +0900

    [CREATE] CREATE HEADER UI

---
+++ src/components/Header/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const LogoShape = styled.img`
+    width: 8%;
+`
+
+export const LogoName = styled.img`
+    width: 18%;
+`
+
+export const Sign = styled.div`
+    display: flex;
+    margin-right: 50px;
+`
+
+export const SignIn = styled.p`
+    padding-right: 10px;
+    cursor: pointer;
+    color: #5D5D5D;
+    &:hover{
+        color: #141414;
+    }
+    &:active{
+        color: #5D5D5D;
+    }
+`
+
+export const SignUp = styled.p`
+    padding-left: 10px;    
+    cursor: pointer;
+    color: #5D5D5D;
+    &:hover{
+        color: #141414;
+    }
+    &:active{
+        color: #5D5D5D;
+    }
+`
+
