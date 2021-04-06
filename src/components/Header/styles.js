7ca1d34a2537937e658b5cd62b859b7fb6341c05
seockhun <hun00719@gmail.com>
2021-04-06 19:54:10 +0900

    [ADD] HEADER UI

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,6 +4,9 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: fixed;
+    width: 100%;
+    z-index: 1;
 `
 
 export const LogoShape = styled.img`
