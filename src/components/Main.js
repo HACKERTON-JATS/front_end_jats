d17b9df20769094d21b2700ed184d43e2b44627d
seockhun <hun00719@gmail.com>
2021-04-17 18:53:32 +0900

    [ADD] MAIN UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,20 @@
 import Header from "./Header/Header";
 // import { Link } from "react-router-dom";
-// import FeedMain from "./Feed/FeedMain";
-import Write from "./Write/Write";
+import FeedMain from "./Feed/FeedMain";
+// import Write from "./Write/Write";
 
 const Main = () => {
     return (
         <>
             <Header />
-            <Write />
-            {/* {
+            {/* <Write /> */}
+            {
                 [...Array(10)].map((i, index) => {
                     return (
                         <FeedMain />
                     );
                 })
-            } */}
+            }
         </>
     )
 }
